5cc71e367030729314fd43499f0efadd3faa9923
Nikhil Gajendrakumar <nikhil.g@gatech.edu>
2017-06-24 13:16:11 -0700

    Extended-Kalman-Filter: Implement root mean squared error function

--- Bicycle-tracker-with-EKF/src/tools.cpp
+++ Bicycle-tracker-with-EKF/src/tools.cpp
@@ -15,6 +15,26 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	if(estimations.size() != ground_truth.size()
+			|| estimations.size() == 0){
+		cout << "Invalid estimation or ground_truth data" << endl;
+		return rmse;
+	}
+
+	for(unsigned int i=0; i < estimations.size(); ++i){
+		VectorXd residual = estimations[i] - ground_truth[i];
+		residual = residual.array()*residual.array();
+		rmse += residual;
+	}
+	rmse = rmse/estimations.size();
+	rmse = rmse.array().sqrt();
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
