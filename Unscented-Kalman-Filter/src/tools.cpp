fb3d9814c08d642b40416a2d7611498fdaccf468
Nikhil Gajendrakumar <nikhil.g@gatech.edu>
2017-07-11 06:44:00 +0500

    UKF: calculate Root mean squared error
    RMSE between estimations and ground truth
    
    Signed-off-by: Nikhil Gajendrakumar <ngajendr@usc.edu>

--- Unscented-Kalman-Filter/src/tools.cpp
+++ Unscented-Kalman-Filter/src/tools.cpp
@@ -11,8 +11,17 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+	VectorXd res(4);
+	res << 0, 0, 0, 0;
+
+	if (estimations.size() != ground_truth.size() ||
+		estimations.size() == 0) {
+		cout << "Invalid estimations or ground_truth data\n";
+		return res;
+	}
+	for (int i = 0; i < estimations.size(); i++)
+		res += square(estimations[i].array() - ground_truth[i].array());
+	res /= estimations.size();
+	
+	return sqrt(res.array());
 }
