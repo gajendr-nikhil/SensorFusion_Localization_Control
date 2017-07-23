73b18164315086a9783c5e3954ae515491546e48
Nikhil Gajendrakumar <nikhil.g@gatech.edu>
2017-07-23 03:47:42 +0040

    PID-Control: Implement PD controller
    Car still gets stuck at 32mph because of high proportional gain
    Signed-off-by: Nikhil Gajendrakumar <ngajendr@usc.edu>

--- PID-Control/src/PID.cpp
+++ PID-Control/src/PID.cpp
@@ -15,14 +15,16 @@ void PID::Init(double Kp, double Ki, double Kd) {
 	this->Ki = Ki;
 	this->Kd = Kd;
 
-	this->p_error = 0;
+	p_error = 0;
+	d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
-	this->p_error = cte;
+	d_error = cte - p_error;
+	p_error = cte;
 }
 
 double PID::TotalError() {
-	return -Kp * this->p_error;
+	return -Kp * p_error - Kd * d_error;
 }
 
