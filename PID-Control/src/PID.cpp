987890884d91323270ce3ab5bee6e5354b0eab16
Nikhil Gajendrakumar <nikhil.g@gatech.edu>
2017-07-23 04:19:02 -0700

    PID-Control: Implemented full PID controller
    Car steers off track. Need to tune parameters.
    Signed-off-by: Nikhil Gajendrakumar <ngajendr@usc.edu>

--- PID-Control/src/PID.cpp
+++ PID-Control/src/PID.cpp
@@ -17,14 +17,16 @@ void PID::Init(double Kp, double Ki, double Kd) {
 
 	p_error = 0;
 	d_error = 0;
+	i_error = 0;
 }
 
 void PID::UpdateError(double cte) {
 	d_error = cte - p_error;
 	p_error = cte;
+	i_error += cte;
 }
 
 double PID::TotalError() {
-	return -Kp * p_error - Kd * d_error;
+	return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
 
