e875d9f6637cb731d959cef480e68cf46bf73557
Nikhil Gajendrakumar <nikhil.g@gatech.edu>
2017-07-23 03:19:14 +0010

    PID-Control: Implemented P controller
    As expected, car overshoots with respect to reference trajectory.
    Car gets stuck as soon as speed reaches 30mph
    Signed-off-by: Nikhil Gajendrakumar <ngajendr@usc.edu>

--- PID-Control/src/PID.cpp
+++ PID-Control/src/PID.cpp
@@ -11,11 +11,18 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+
+	this->p_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+	this->p_error = cte;
 }
 
 double PID::TotalError() {
+	return -Kp * this->p_error;
 }
 
